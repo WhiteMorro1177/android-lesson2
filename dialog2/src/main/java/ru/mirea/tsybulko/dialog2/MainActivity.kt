5bd578c5e43c7a38831f9a3b39216f33328ad2c5
WhiteMorro1177 <jackforest228@gmail.com>
2023-03-06 13:01:52 +0300

    added date picker dialog (need testing)

--- dialog2/src/main/java/ru/mirea/tsybulko/dialog2/MainActivity.kt
+++ dialog2/src/main/java/ru/mirea/tsybulko/dialog2/MainActivity.kt
@@ -1,20 +1,49 @@
 package ru.mirea.tsybulko.dialog2
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // create main activity
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // bind components
         val timeTextView = findViewById<TextView>(R.id.timeTextView)
-        val currentTime = LocalDateTime.now()
-        timeTextView.text = "${currentTime.hour}:${if (currentTime.minute.toString().length < 2) ("0${currentTime.minute}") else (currentTime.minute)}"
+        val dateTextView = findViewById<TextView>(R.id.dateTextView)
+        // val progressTextView = findViewById<TextView>(R.id.progressTextView)
+
+
+        val time: String = getCurrentTime("hh:mm")
+        val date: String = getCurrentDate("dd.MM.yyyy")
+        timeTextView.text = time
+        dateTextView.text = date
+    }
+
+    private fun getCurrentTime(format: String): String {
+
+        // multiply format with time
+
+        val current = LocalDateTime.now()
+        return "${current.hour}:${if (current.minute.toString().length < 2) ("0${current.minute}") else (current.minute)}"
+    }
+
+    private fun getCurrentDate(format: String): String {
+
+        // multiply format with dates
+
+        val current = LocalDate.now()
+        val currentDay =
+            if (current.dayOfMonth.toString().length < 2) (current.dayOfMonth.toString()) else ("0${current.dayOfMonth}")
+        val currentMonth =
+            if (current.month.toString().length < 2) (current.month.toString()) else ("0${current.month}")
+
+        return "$currentDay.$currentMonth.${current.year}"
     }
 
     fun onClickShowDialog(view: View) {
@@ -25,5 +54,14 @@ class MainActivity : AppCompatActivity() {
     fun showTimeDialog(view: View) {
         val dialog = MyTimeDialogFragment(this)
         dialog.show()
+    }
+
+    fun showDateDialog(view: View) {
+        val dialog = MyDateDialogFragment(this)
+        dialog.show()
+    }
+
+    fun showProgressDialog(view: View) {
+        TODO("learn things about progress dialog")
     }
 }
--- dialog2/src/main/java/ru/mirea/tsybulko/dialog2/MyDateDialogFragment.kt
+++ dialog2/src/main/java/ru/mirea/tsybulko/dialog2/MyDateDialogFragment.kt
@@ -1,4 +1,24 @@
 package ru.mirea.tsybulko.dialog2
 
-class MyDateDialogFragment {
+import android.app.DatePickerDialog
+import android.content.Context
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MyDateDialogFragment(private val ctx: Context) : DatePickerDialog(ctx) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        this.setOnDateSetListener { _, year, month, dayOfMonth ->
+            val dateTextView: TextView = (ctx as AppCompatActivity).findViewById(R.id.dateTextView)
+
+            val date = "${if (dayOfMonth.toString().length < 2) ("0$dayOfMonth") else (dayOfMonth)}." +
+                    "${if (month.toString().length < 2) ("0$month") else (month)}." +
+                    "$year"
+
+            date.also { dateTextView.text = it }
+        }
+    }
 }
